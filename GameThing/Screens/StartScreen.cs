d990e25355e072d06bc8d0390e55ce9145e960db
Craig Fleming <rythos42@gmail.com>
2019-08-27 20:23:19 -0700

    Added a little instruction

--- GameThing/Screens/StartScreen.cs
+++ GameThing/Screens/StartScreen.cs
@@ -14,6 +14,7 @@ namespace GameThing.Screens
 		private readonly Button signIn = new Button("Sign In");
 		private readonly Button createMatch = new Button("Create Match");
 		private readonly Button joinMatch = new Button("Join Match");
+		private SpriteFont font;
 
 		private ApplicationData appData;
 
@@ -34,6 +35,8 @@ namespace GameThing.Screens
 			signIn.LoadContent(content, contentManager, graphicsDevice);
 			createMatch.LoadContent(content, contentManager, graphicsDevice);
 			joinMatch.LoadContent(content, contentManager, graphicsDevice);
+
+			font = content.Font;
 		}
 
 		public void Update(GameTime gameTime)
@@ -81,6 +84,16 @@ namespace GameThing.Screens
 				createMatch.Draw(spriteBatch, 40, 160);
 				joinMatch.Draw(spriteBatch, 40, 280);
 			}
+
+			spriteBatch.DrawString(font, "Welcome to the MVP of GameThing!", new Vector2(500, 40), Color.Black);
+			spriteBatch.DrawString(font, "You play as a team of 5 characters.", new Vector2(500, 90), Color.Black);
+			spriteBatch.DrawString(font, "Each character has a deck of 8 cards, with 4 cards in hand at a time.", new Vector2(500, 120), Color.Black);
+			spriteBatch.DrawString(font, "Characters can play 2 cards and move 5 in their turn.", new Vector2(500, 150), Color.Black);
+			spriteBatch.DrawString(font, "Once you play a card or move a character, you can't choose another this turn.", new Vector2(500, 180), Color.Black);
+			spriteBatch.DrawString(font, "When you choose to be finished for a turn, press New Turn and your opponent will play.", new Vector2(500, 210), Color.Black);
+			spriteBatch.DrawString(font, "After one side looses all characters, the other side wins!", new Vector2(500, 240), Color.Black);
+
+			spriteBatch.DrawString(font, "The MVP only has play versus another person and is turn-based.", new Vector2(500, 290), Color.Black);
 
 			spriteBatch.End();
 		}
